e0575888314955fb58e735b4ab11b990d3208e50
hidhmat <hidhmatgroups@gmail.com>
2023-11-11 13:23:23 +0530

    ready to deploy

---
+++ server/config/collection.js
@@ -0,0 +1,13 @@
+module.exports = {
+    verifyCollection: 'verifydata',
+    userCollection: 'userdata',
+    eventCollection: 'events',
+    bookCollection: 'Eventbooking',
+    fineCollection: 'fine',
+    otCollection: 'ot',
+    teCollection: 'te',
+    withdrawCollection: 'withdraw',
+    salaryCollection: 'salary',
+    captainCollection: 'captain',
+    notificationCollection: 'notification',
+}
